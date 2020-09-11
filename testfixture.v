cd26c92a4cd3157ec2b27a7b59f9463409ab7f70
strong-Ting <s8527502987@gmail.com>
2020-09-11 14:14:21 +0800

    -synthesis
    -area 29359  < 270,000
    -S

--- testfixture.v
+++ testfixture.v
@@ -1,6 +1,6 @@
 `timescale 1ns/10ps
 `define CYCLE      10.0          	  // Modify your clock period here
-`define SDFFILE    "./syn/CONV_syn.sdf"	  // Modify your sdf file name
+`define SDFFILE    "./CONV_syn.sdf"	  // Modify your sdf file name
 `define End_CYCLE  100000000              // Modify cycle times once your design need more cycle times!
 
 `define PAT        "./dat_grad/cnn_sti.dat"                 // Modify your "dat" directory path
